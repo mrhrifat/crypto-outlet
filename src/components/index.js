c05089fc7c0a147c26ef967252bfa0f7c87385ed
mrhrifat <mrhrifat383759@gmail.com>
2021-11-30 01:13:18 +0600

    Export all components & import to root with destructure

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+export {
+    default as Navbar
+}
+from './Navbar'
+
+export {
+    default as HomePage
+}
+from './HomePage'
+
+export {
+    default as Exchanges
+}
+from './Exchanges'
+
+export {
+    default as Cryptocurrencies
+}
+from './Cryptocurrencies'
+
+export {
+    default as CryptoDetails
+}
+from './CryptoDetails'
+
+export {
+    default as News
+}
+from './News'
