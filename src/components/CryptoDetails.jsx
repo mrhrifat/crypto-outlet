9d1e75bb239050a0b0d80c1ee4ebc6a901bee20a
mrhrifat <mrhrifat383759@gmail.com>
2021-11-30 01:14:41 +0600

    Upload initial components

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoDetails = () => {
+    return (
+        <div>
+            CryptoDetails
+        </div>
+    )
+}
+
+export default CryptoDetails
---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cryptocurrencies = () => {
+    return (
+        <div>
+            Cryptocurrencies
+        </div>
+    )
+}
+
+export default Cryptocurrencies
