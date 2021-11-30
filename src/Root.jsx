1efea6c9fcd02e913f1c32d956b377c1d4efb3c0
mrhrifat <mrhrifat383759@gmail.com>
2021-12-01 02:17:02 +0600

    Reset code for private key issue

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
 import './App.css'
-import { Navbar,HomePage,Exchanges,Cryptocurrencies,CryptoDetails,News } from './components'
+import { Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
 // import Navbar from './components/Navbar'
 
 const Root = () => {
@@ -15,17 +15,29 @@ const Root = () => {
                 <Layout>
                     <div className='routes'>
                         <Routes>
-                            <Route path='/' element={<HomePage/>}/>
-                            <Route path='/exchanges' element={<Exchanges/>}/>
-                            <Route path='/cryptocurrencies' element={<Cryptocurrencies/>}/>
-                            <Route path='/crypto/:coinId' element={<CryptoDetails/>}/>
-                            <Route path='/news' element={<News/>}/>
+                            <Route path='/' element={<HomePage />} />
+                            <Route path='/exchanges' element={<Exchanges />} />
+                            <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                            <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+                            <Route path='/news' element={<News />} />
                         </Routes>
                     </div>
                 </Layout>
-            </div>
-            <div className='footer'></div>
 
+                <div className='footer'>
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Crypto Outlet
+                        <br />
+                        All Right Reserved || 2021 Mrh Rifat
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
+            </div>
         </div>
     )
 }
