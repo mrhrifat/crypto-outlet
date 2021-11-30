11d08f4d4117bc4c5d519225fc938e5e664fdc0b
mrhrifat <mrhrifat383759@gmail.com>
2021-12-01 02:16:28 +0600

    Wrapped with React Redux Provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import 'antd/dist/antd.css'
+import store from './app/store'
 import Root from "./Root";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Root />
+        <Provider store={store}>
+          <Root />
+        </Provider>
       </BrowserRouter>
     </div>
   );
 }
-
 export default App;
