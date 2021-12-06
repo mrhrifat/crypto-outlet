55c0c23315769021339c4e651d04f8fafd835767
mrhrifat <mrhrifat383759@gmail.com>
2021-12-07 00:06:18 +0600

    Add Loadeer Components

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -3,6 +3,7 @@ import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { Card, Row, Col, Input } from 'antd'
 import { useGetCryptosQuery } from '../services/cryptoApi'
+import Loader from './Loader'
 
 const Cryptocurrencies = ({ simplified }) => {
     const count = simplified ? 10 : 100
@@ -18,7 +19,7 @@ const Cryptocurrencies = ({ simplified }) => {
         setCryptos(filteredData)
     }, [cryptosList, searchTerm])
 
-    if (isFetching) return 'Loading...'
+    if (isFetching) return <Loader/>
     return (
         <>
             {!simplified && (
