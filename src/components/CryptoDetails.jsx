5dfb2a4d308090623395147805a9c033adec5faa
mrhrifat <mrhrifat383759@gmail.com>
2021-12-06 19:56:14 +0600

    Visible Line Chart

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -64,7 +64,7 @@ const CryptoDetails = () => {
                 )}
             </Select>
 
-            {/* <LineChart coinHistory={coinHistory} currentPrice={cryptoDetails?.price} coinName={cryptoDetails?.name} /> */}
+            <LineChart coinHistory={coinHistory} currentPrice={cryptoDetails?.price} coinName={cryptoDetails?.name} />
 
             <Col className='stats-container'>
                 <Col className='coin-value-statistics'>
