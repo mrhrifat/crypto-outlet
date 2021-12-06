87072916342252e9baa4c15587304e4cdf330969
mrhrifat <mrhrifat383759@gmail.com>
2021-12-07 00:05:18 +0600

    Add Loadeer Components

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -6,6 +6,7 @@ import { Row, Col, Typography, Select } from 'antd'
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi'
 import LineChart from './LineChart'
+import Loader from './Loader'
 const { Title, Text } = Typography
 const { Option } = Select
 
@@ -21,7 +22,7 @@ const CryptoDetails = () => {
     // console.log(cryptoDetails?.description)
     // console.log(data?.data?.coin.name)
 
-    if (isFetching) return 'Loading...'
+    if (isFetching) return <Loader/>
 
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
