1594f81df8b4363ac9a37d416c189383750e98f1
mrhrifat <mrhrifat383759@gmail.com>
2021-12-06 19:55:26 +0600

    Resolve ccategory error with ChartJS

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import { Line } from 'react-chartjs-2'
 import { Row, Col, Typography } from 'antd'
-const { Title } = Typography
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+// const { Title } = Typography
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
     const coinPrice = []
     const coinTimestamp = []
-    
+
     for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
         coinPrice.push(coinHistory?.data?.history[i].price);
         coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
@@ -40,16 +61,16 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
     return (
         <>
             <Row className='chart-header'>
-                <Title level={2} className='chart-title'>
+                <Typography.Title level={2} className='chart-title'>
                     {coinName} Price Chart
-                </Title>
+                </Typography.Title>
                 <Col className='price-container'>
-                    <Title level={5} className='price-change'>
+                    <Typography.Title level={5} className='price-change'>
                         {coinHistory?.data?.change}%
-                    </Title>
-                    <Title level={5} className='current-price'>
+                    </Typography.Title>
+                    <Typography.Title level={5} className='current-price'>
                         Current {coinName} Price : $ {currentPrice}
-                    </Title>
+                    </Typography.Title>
                 </Col>
             </Row>
             <Line data={data} options={options} />
